2bc59609ae0f9caa11c5a09ca9194eab0c5339fe
Rahul Kumar Mishra <rahulkumar.mishra17825@marwadieducation.edu.in>
2023-02-16 00:06:34 +0530

    last changes

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ let hamberger = document.querySelector(".hamberger");
 let cancelBtn = document.querySelector(".cancel-btn");
 let mobileNav = document.querySelector(".mobile-nav");
 let p = document.querySelectorAll("#b");
-console.log(p)
-p.forEach((ele=>{
-    ele.addEventListener("click",e=>{
-        console.log(p)
-        alert(p)
-        // mobileNav.classList.remove('open');
-    })
-}))
-hamberger.addEventListener('click', function(){
-    mobileNav.classList.add('open');
+
+hamberger.addEventListener("click", function () {
+  mobileNav.classList.add("open");
 });
-cancelBtn.addEventListener('click', function(){
-    mobileNav.classList.remove('open');
+cancelBtn.addEventListener("click", function () {
+  mobileNav.classList.remove("open");
 });
