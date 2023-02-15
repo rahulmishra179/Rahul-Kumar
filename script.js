4dec55ab14ef8455d009853957aba0175c82d165
Rahul Kumar Mishra <rahulkumar.mishra17825@marwadieducation.edu.in>
2023-02-15 23:20:08 +0530

    responsive for mobile

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ console.log("Hii Rahul");
 let hamberger = document.querySelector(".hamberger");
 let cancelBtn = document.querySelector(".cancel-btn");
 let mobileNav = document.querySelector(".mobile-nav");
-
+let p = document.querySelectorAll("#b");
+console.log(p)
+p.forEach((ele=>{
+    ele.addEventListener("click",e=>{
+        console.log(p)
+        alert(p)
+        // mobileNav.classList.remove('open');
+    })
+}))
 hamberger.addEventListener('click', function(){
     mobileNav.classList.add('open');
 });
